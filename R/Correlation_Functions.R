a453c303cd863c9e7f0ba9a5174eebab6cf08fb3
Steven J. Pierce <pierces1@msu.edu>
2020-04-14 20:58:42 -0400

    Updating ci.rp() to use t-distribution instead of z-distribution.

--- R/Correlation_Functions.R
+++ R/Correlation_Functions.R
@@ -129,17 +129,17 @@ ci.rpc <- function(r, se, conf.level = 0.95, rn = NULL) {
 #'   frame returned by this function.
 #'
 #' @details This function computes the upper and lower bounds of an approximate,
-#'   two-sided confidence interval based on the z-distribution, with the p-value
-#'   also based on the z-test statistic. Additional statistics are also
+#'   two-sided confidence interval based on the t-distribution, with the p-value
+#'   also based on the t-test statistic. Additional statistics are also
 #'   computed, namely the p-value, s-value, BFB, and posterior probability of
 #'   H1.
 #'
 #' @return A data frame containing the correlation coefficient, standard error,
-#'   confidence interval limits, z-statistic, p-value, s-value, BFB, and
+#'   confidence interval limits, t-statistic, p-value, s-value, BFB, and
 #'   posterior probability of H1.
 #'
 #' @importFrom assertthat assert_that
-#' @importFrom stats pnorm
+#' @importFrom stats pt
 #' @importFrom mvtnorm rmvnorm
 #'
 #' @seealso \code{\link{p2s}} for s-values, \code{\link{p2bfb}} for BFBs, and
@@ -194,12 +194,12 @@ ci.rp <- function(r, se, n, conf.level = 0.95, rn = NULL) {
   z       <- Fisher_r2z(r)  # correlation after Fisher's z-transform
   sez     <- 1/sqrt(n - 3)  # se after Fisher's z-transform
   # CI limits after back-transformation from z-scores back to r.
-  ci_lo   <- Fisher_z2r(z - qnorm((1 + conf.level)/2)*sez)
-  ci_up   <- Fisher_z2r(z + qnorm((1 + conf.level)/2)*sez)
-  # Calculate a z statistic from which to get a p-value for H0: r = 0.
-  ztest <- r/se
-  # Next convert that z test statistic to a lower-tail p-value
-  p <- pnorm(q = ztest)
+  ci_lo   <- Fisher_z2r(z - qt((1 + conf.level)/2, df = n - 2)*sez)
+  ci_up   <- Fisher_z2r(z + qt((1 + conf.level)/2, df = n - 2)*sez)
+  # Calculate a t statistic from which to get a p-value for H0: r = 0.
+  ttest <- r/se
+  # Next convert that t test statistic to a lower-tail p-value
+  p <- pt(q = ttest, df = n - 2)
   # Calculate a two-sided p-value, plus additional measures.
   Pval <- 2*min(p, 1 - p)
   Sval <- p2s(Pval)
@@ -207,7 +207,7 @@ ci.rp <- function(r, se, n, conf.level = 0.95, rn = NULL) {
   PPH1 <- p2pp(Pval)
   # Combine results into a data frame for nice printing.
   ci = data.frame(Cor = r, SE = se, CI.LL = ci_lo, CI.UL = ci_up,
-                  Z = ztest, Pval, Sval, BFB, PPH1)
+                  t = ttest, Pval, Sval, BFB, PPH1)
   row.names(ci) <- rn
   return(ci)
 }
